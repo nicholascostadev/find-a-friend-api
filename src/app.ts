e91ca747a813d1d5560f198249a5ea997de4cd67
Nicholas Costa <nicholascostadev@gmail.com>
2025-04-29 16:35:36 -0300

    feat: add global exception handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import fastify from "fastify";
 
+import { ZodError } from "zod";
+import { Exception } from "./exceptions/exception";
 import { env } from "./lib/env";
 
 const app = fastify({
@@ -13,4 +15,24 @@ app.listen({ port: env.PORT }, (err) => {
   }
 
   app.log.info(`HTTP server running! http://localhost:${env.PORT}`);
+});
+
+app.setErrorHandler((error, _, reply) => {
+	if (error instanceof ZodError) {
+		return reply
+			.status(400)
+			.send({ message: "Validation error.", issues: error.format() });
+	}
+
+  if (error instanceof Exception) {
+    return reply.status(error.statusCode).send({ message: error.message });
+  }
+
+	if (env.NODE_ENV !== "production") {
+		console.error(error);
+	} else {
+		// TODO: add a log to an external tool like DataDog/Sentry
+	}
+
+	return reply.status(500).send({ message: "Internal server error." });
 });
